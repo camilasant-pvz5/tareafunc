79b0949999726b78ed578b23810e6650f69f7558
camila santibañez <cspavez5@gmail.com>
2024-03-17 00:15:12 -0300

    ultimos arreglos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,13 @@
-var azul = document.getElementById("azul");
-var rojo = document.getElementById("rojo");
-var amarillo = document.getElementById("amarillo");
-var verde = document.getElementById("verde");
-
-azul.addEventListener("click", function() {
-    cambiarColor(azul);
-});
-
-rojo.addEventListener("click", function() {
-    cambiarColor(rojo);
-});
-
-amarillo.addEventListener("click", function() {
-    cambiarColor(amarillo);
-});
-
-verde.addEventListener("click", function() {
-    cambiarColor(verde);
-});
-
-function cambiarColor(elemento) {
-    elemento.style.backgroundColor = "black";
-}
 
 
-function pintar(elemento, color = 'green') {
-    elemento.style.backgroundColor = color;
+// js relacionado al pintar 
+
+function pintar(event, color = 'green') {
+    event.target.style.backgroundColor = color;
 }
 
 const ele = document.getElementById("ele1");
-ele.addEventListener("click", function() {
-    pintar(ele, 'yellow');
+ele.addEventListener("click", function(event) {
+    pintar(event, 'yellow');
 });
-
-
 
